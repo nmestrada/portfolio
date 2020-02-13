0c3d7f8d574cbec2ab5629cb5b4938cfe4d30592
Natalie Estrada <nmestrad@gmail.com>
2020-02-12 23:07:57 -0600

    gatsby build

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Navbar from "./Navbar"
+import Navbar from "./navbar"
 import "./layout.css"
 
 import Container from 'react-bootstrap/Container'
