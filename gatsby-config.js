d3d1f5d0caf8a96a2bf2c571066b455b8afb989d
Natalie Estrada <nmestrad@gmail.com>
2020-03-05 16:21:05 -0600

    added flavicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Natalie Estrada`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Full Stack Software Engineer`,
+    author: `@NatalieEstrada`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,10 +24,16 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/seedling.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
+    {
+        resolve: `gatsby-plugin-google-analytics`,
+        options: {
+          trackingId: "UA-149334253-1",
+        },
+      },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
